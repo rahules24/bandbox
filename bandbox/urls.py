4733862d419174800754a17c276dd27b2f3a1c68
Rahul <scarletteenager@outlook.com>
2022-12-05 03:37:32 +0530

    added form

--- bandbox/urls.py
+++ bandbox/urls.py
@@ -22,4 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="Index"),
+    path("slip/", views.slip, name="Slip"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- bandbox/views.py
+++ bandbox/views.py
@@ -1,21 +1,26 @@
 # from django.shortcuts import HttpResponse
 from django.shortcuts import render
 from .models import Item, Slip
-import math
 
 
 def index(request):
     allitems = []
-    # slipid = []
-    # slipids = Slip.objects.values('slip_id')
-    # ids = {item['slip_id'] for item in slipids}
     catitems = Item.objects.values('category')
     cats = {item['category'] for item in catitems}
     for cat in cats:
         item = Item.objects.filter(category=cat)
         allitems.append(item)
-    # for i in ids:
-    #     num = Slip.objects.filter(slip_id=i)
-    #     slipid.append(num)
-    # # print(slipid)
     return render(request, 'index.html', {'allitems': allitems})
+
+
+def slip(request):
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJSON', '')
+        address = request.POST.get('address', '')
+        amount = request.POST.get('amount', '')
+        phone = request.POST.get('phone', '')
+        date = request.POST.get('date', '')
+        duedate = request.POST.get('due-date', '')
+        newslip = Slip(items_json=items_json, amount=amount, address=address, phone=phone, date=date, due_date=duedate)
+        newslip.save()
+    return render(request, 'slip.html')
